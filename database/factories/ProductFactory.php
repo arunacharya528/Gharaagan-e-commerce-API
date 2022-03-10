c23f39fa1ee032df4a2d1c504efa5bd25fbf791b
Arun <arunacharya255@gmail.com>
2022-03-10 18:53:36 +0545

    Initial commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->paragraph(),
+            'SKU' => $this->faker->uuid(),
+            'price' => rand(1000, 2000)
+        ];
+    }
+}
